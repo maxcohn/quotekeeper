3a2be20d79c1795a5795eb1af28fc2a93c2ca19c
Ubuntu <ubuntu@ip-172-31-41-125.us-east-2.compute.internal>
2019-07-24 23:07:27 +0000

    Still fixing merge conflict...

--- quotekeeper/static/script.js
+++ quotekeeper/static/script.js
@@ -1,42 +1,3 @@
-<<<<<<< Updated upstream
-
-function sendQuote(url, data){
-    fetch(url, {
-        method: 'POST', // or 'PUT'
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      })
-}
-
-$(document).ready(function(){
-    $("#send").click(function(){
-        console.log('Sending POST');
-        var data = {
-            text: $("#input-text").val(),
-            author: $("#input-author").val()
-        };
-
-		// check if any text is empty
-        if(data.text === '' || data.author === ''){
-			alert('Text or author is empty');
-        	return;
-        }
-    
-        fetch('/newquote', {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(data), // data can be `string` or {object}!
-            headers:{
-              'Content-Type': 'application/json'
-            }
-        });
-
-        location.reload(true);
-    });
-});
-=======
-
 function sendQuote(url, data){
     fetch(url, {
         method: 'POST', // or 'PUT'
@@ -71,4 +32,3 @@ $(document).ready(function(){
 	alert("Quote subitted!!");
     });
 });
->>>>>>> Stashed changes
